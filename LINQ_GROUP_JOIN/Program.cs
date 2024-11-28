5184b7950a4688a20fb6f30d629f9387ba9a4ef9
Melisa Kılıç <109312556+melitata@users.noreply.github.com>
2024-11-28 03:02:32 +0300

    Add files via upload

---
+++ LINQ_GROUP_JOIN/Program.cs
@@ -0,0 +1,40 @@
+﻿using GroupJoin;
+using System.Threading.Channels;
+
+public class Program 
+{
+  public static void Main(string[] args)
+  {
+    List<Student> student =new List<Student>()
+    {
+        new Student() { StudentID = 1, StudentName = "Ali", ClassID = 1 },
+        new Student() { StudentID = 2, StudentName = "Ayşe", ClassID = 2 },
+        new Student() { StudentID = 3, StudentName = "Mehmet", ClassID = 1 },
+        new Student() { StudentID = 4, StudentName = "Fatma", ClassID = 3 },
+        new Student() { StudentID = 5, StudentName = "Ahmet", ClassID = 2 }
+    };
+    List<Classes> classes = new List<Classes>()
+    {
+        new Classes() { ClassID = 1, ClassName = "Matematik" },
+        new Classes() { ClassID = 2, ClassName = "Türkçe" },
+        new Classes() { ClassID = 3, ClassName = "Kimya" }
+    };
+
+     var comolokko= student.GroupJoin
+     (                               classes,
+                                     s => s.ClassID,
+                                     c => c.ClassID,
+                                     (student, classes) => new
+                                     {
+                                       StudentNam = student.StudentName,
+                                       ClassNam = classes.Select(x => x.ClassName).FirstOrDefault()
+                                     }
+     );
+        foreach (var item in comolokko)
+        {
+            Console.WriteLine($"Öğrenci Adı: " + item.StudentNam + "\t*Ders Adı: " + item.ClassNam);
+        }
+        
+    }
+
+}
---
+++ LINQ_GROUP_JOIN/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupJoin
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+         public int ClassID { get; set; }
+          public string StudentName { get; set; }
+    }
+}
